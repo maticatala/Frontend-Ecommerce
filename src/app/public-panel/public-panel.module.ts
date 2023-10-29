24ddc3283b935e3ca04ff1511e79950f04b4c7b3
matias <matiasncatala@gmail.com>
2023-10-29 14:03:17 -0300

    Subiendo repositorio

---
+++ src/app/public-panel/public-panel.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PublicPanelRoutingModule } from './public-panel-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PublicPanelRoutingModule
+  ]
+})
+export class PublicPanelModule { }
