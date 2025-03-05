a55c339ff0d88f2bf0a4bdef9ac27aca01eb080d
Matias Catala <matiasncatala@gmail.com>
2025-03-05 00:33:34 -0300

    last commit

--- src/app/public-panel/services/contact.service.ts
+++ src/app/public-panel/services/contact.service.ts
@@ -3,17 +3,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ContactForm, EmailResponse } from '../interfaces/contact-form.interface';
+import { environment } from 'src/app/environments/environments';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  private baseUrl = 'http://localhost:3000' //URL backend
+  private baseURL = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
   sendContactForm(formData: ContactForm): Observable<EmailResponse> {
-    return this.http.post<EmailResponse>(`${this.baseUrl}/contact`, formData);
+    return this.http.post<EmailResponse>(`${this.baseURL}/contact`, formData);
   }
 }
