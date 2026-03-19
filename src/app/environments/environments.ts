d0d6a7d03ee3535dbd44980f7b02af01f4e87194
Matias Catala <matiasncatala@gmail.com>
2026-03-18 22:18:03 -0300

    changing environment

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,6 @@
 declare const window: any;
 
 export const environment = {
-  baseUrl: window.__env?.baseUrl || 'http://localhost:3000',
+  baseUrl:
+    window.__env?.baseUrl || 'http://beyondlimitsbackend.matiascatala.com',
 };
